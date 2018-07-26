365a4150ba04762253ec36c622308c0fe28d86ca
ManspergerMichael <ManspergerMichael@gmail.com>
2018-07-26 11:08:28 -0700

    Web config

--- Program.cs
+++ Program.cs
@@ -19,7 +19,10 @@ namespace Belt_Exam
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
+                .UseIISIntegration()
                 .Build();
     }
 }
